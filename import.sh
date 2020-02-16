221ff0545c72ea24cca3ed2fd242d6307243bc1c
Michael Soh <sohmc@users.noreply.github.com>
2020-02-16 16:47:13 -0500

    Parsedvi import.sh !

--- import.sh
+++ import.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-# read file
-if [[ -f $1 ]]; then
-    CONFIG_FILE=$1
-fi
-
-delimiter=:
+DELIMITER=:
 
 while IFS=$'\n' read -r line; do
-    s=$line$delimiter
+    s=$line$DELIMITER
     array=();
     while [[ $s ]]; do
-        array+=( "${s%%"$delimiter"*}" );
-        s=${s#*"$delimiter"};
+        array+=( "${s%%"$DELIMITER"*}" );
+        s=${s#*"$DELIMITER"};
     done;
 
-    echo ${array[0]}
+    # Variablize it so we know what we're dealing with
+    ACCOUNT=${array[0]}
+    ISSUER=${array[1]}
+    SECRET=${array[2]}
+    TOUCH_TRUE=${array[3]}
+    OPTIONS=${array[4]}
+
+    # if the account name and the secret are not set,
+    # silently skip
+    if [[ ! -z $ACCOUNT ]] && [[ ! -z $SECRET ]]; then
+        echo $ACCOUNT
+        echo $ISSUER
+        echo $SECRET
+        echo $TOUCH_TRUE
+        echo $OPTIONS
+    fi
 done < ./$1
 
 
